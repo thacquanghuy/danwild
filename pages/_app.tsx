a3847a1ac8bf1443c9ffc2544bd3ffcc3576f40f
huyntq <ntquanghuy2705@gmail.com>
2022-03-05 17:59:02 +0700

    add spinner

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
-import { Navbar, MetaInfo, Footer, Cart } from '../components/common'
+import { Navbar, MetaInfo, Footer, Cart, Spinner } from '../components/common'
 import { CartContextProvider } from '../context/cart'
 import { useRouter } from 'next/router'
 import { ProductContextProdiver } from '../context/product'
+import React from 'react'
 
 import 'animate.css'
 
@@ -12,20 +13,38 @@ function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter()
   const pathname = router?.pathname
   const whitedList = ['/', '/oil', '/candle']
+  const [loading, setLoading] = React.useState(true)
+
+  React.useEffect(() => {
+    const inter = setInterval(() => {
+      setLoading(false)
+    }, 500)
+
+    return () => clearInterval(inter)
+  }, [])
+
   return (
     <CartContextProvider>
       <ProductContextProdiver>
-        <Cart />
-        <Layout>
-          <Navbar navbarWhite={!whitedList.includes(pathname)} />
-          <div className="flex-grow">
-            <Component {...pageProps} />
-          </div>
-          <div>
-            <MetaInfo />
-            <Footer />
-          </div>
-        </Layout>
+        <>
+          <Cart />
+          <Layout>
+            {!loading ? (
+              <>
+                <Navbar navbarWhite={!whitedList.includes(pathname)} />
+                <div className="flex-grow">
+                  <Component {...pageProps} />
+                </div>
+                <div>
+                  <MetaInfo />
+                  <Footer />
+                </div>
+              </>
+            ) : (
+              <Spinner />
+            )}
+          </Layout>
+        </>
       </ProductContextProdiver>
     </CartContextProvider>
   )
